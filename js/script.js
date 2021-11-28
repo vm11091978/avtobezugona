18a00a5bb6ecf682fc2b0afed61cf246025f9690
Vladimir <85496171+vm11091978@users.noreply.github.com>
2021-11-28 20:07:29 +0300

    обновляет файл

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
+// Анимация - выдвигающееся меню
+let menuAdd = document.getElementById('menu-add');
+let menuDel = document.getElementById('menu-del');
+let menu = document.getElementById('menu'); // let menu = document.getElementsByClassName('menu')[0];
+
+let i = 0;
+function animate()
+{
+	if (i <= 250) {
+		menu.style.width = i + 'px';
+		i += 5;
+		setTimeout("animate()", 20);
+	} else if (i = 250) {
+		menu.style.width = '100%';
+		menu.style.overflow = 'visible';
+	}
+}
+
+function deanimate()
+{
+	menu.style.overflow = 'hidden';
+	if (i > 0) {
+		menu.style.width = i + 'px';
+		i -= 5;
+		setTimeout("deanimate()", 20);
+	} else if (i == 0) {
+		menu.removeAttribute('style'); // удалим аттрибуты вместо того, чтобы изменить: menu.style.display = 'none';
+	}
+}
+
+menuAdd.addEventListener('click', function() {
+	if (i == 0) {
+		menuDel.style.zIndex = '3';
+		menu.style.display = 'block';
+		animate();
+	}
+});
+
+menuDel.addEventListener('click', function() {
+	if (i == 250) {
+		window.clearTimeout(animate);
+		menuDel.style.zIndex = '1';
+		deanimate();
+	}
+});
+
+// Ниже весь код для слайдера
 var slideIndex = 1;
 showSlides(slideIndex);
 
